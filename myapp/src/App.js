48378754c43622ccd81b33cb21ed371a23af6b0b
saintnamee <saintnamee@hotmail.com>
2020-02-27 18:09:34 +0700

    add files

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Bear from './componentr/Bear'
+
+function App() {
+  return (
+    <div>
+      Hello word
+      <Bear />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ myapp/src/componentr/Bear.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// const gitBears = async () => {
+
+//     const result = await axios.get('http://localhost/api/bears')
+//     console.log(result.data)
+//     setBears(result.data)
+// }
+
+const Bear = () => {
+
+    const [bears, setBears] = useState({});
+    const [name, setName] = useState('');
+    const [weight, setWeight] = useState(0);
+
+    useEffect(() => {
+        getBears();
+    }, []);
+
+    const getBears = async () => {
+        const result = await axios.get('http://localhost/api/bears');
+        console.log(result.data)
+        setBears(result.data)
+    }
+
+    const addBear = async () => {
+        const result = await axios.post('http://localhost/api/bears', {
+            name,
+            weight
+        });
+        console.log(result.data);
+        getBears()
+    }
+
+    const printBears = () => {
+        if (bears && bears.length)
+            return bears.map((bear, index) => {
+                return (
+                    <li key={index}>
+                        {bear.name} : {bear.weight}
+                    </li>
+                )
+            })
+        else
+            return <h2>No bears</h2>
+    }
+
+    return (
+        <div>
+            Bear
+            {printBears()}
+            <h2>Add Bear</h2>
+            Name: 
+            <input 
+                placeholder="name"
+                type="text"
+                name="name"
+                onChange={ (e)=> setName(e.target.value) }
+                /> <br/>
+            Weight:
+            <input                 
+                type="number"
+                name="weight"
+                onChange={ (e)=> setWeight(e.target.value) }
+                /><br/>
+            <button onClick={addBear}>Add </button>
+        </div>
+    )
+}
+
+export default Bear;
